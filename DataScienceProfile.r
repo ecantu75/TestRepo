63cfe4e7ff1c7510b5587868cfc69eb70ebff745
ecantu75 <31023505+ecantu75@users.noreply.github.com>
2018-01-31 21:25:28 -0600

    Update DataScienceProfile.r

--- DataScienceProfile.r
+++ DataScienceProfile.r
@@ -2,8 +2,8 @@
 profile.cat <- c('Programming', 'math', 'statistics','machineLearning', 'domainExpertise', 'PresentationSkills','dataVis')
  
 #Profile Rank #
-profile.rank <- c(3,4,3,1,5,3,4)
- 
+profile.rank <- c(3,6,3,1,5,3,4)
+ ## Changes On the ranks###
 #Histogram#
  eduardo.frm <- data.frame(profile.cat, profile.rank)
  barplot(eduardo.frm$profile.rank,names.arg=eduardo.frm$profile.cat)
